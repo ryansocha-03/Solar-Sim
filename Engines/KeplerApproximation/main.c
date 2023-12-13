950e2cb844cc64f9fbf8411323deea08782139dc
Ryan <ryansocha03@gmail.com>
2023-12-12 21:29:35 -0500

    Getting wasm array from js file

--- Engines/KeplerApproximation/main.c
+++ Engines/KeplerApproximation/main.c
@@ -1,6 +1,9 @@
 #include <emscripten.h>
 
 EMSCRIPTEN_KEEPALIVE
-int main() {
-    return 1000;
+int* KeplerEngine(int a, int b) {
+    int result[2];
+    result[0] = a;
+    result[1] = b;
+    return result;
 }
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Import the MyModule from the Emscripten-generated file (example.js)
-import  { Module } from './Engines/KeplerApproximation/main.js';
+//import {Module} from './Engines/KeplerApproximation/main.js';
 
 // Set up a callback for when the Emscripten module is initialized
-Module.onRuntimeInitialized = () => {
-    // Call the C function
-    const result = Module._main();
+import init from './Engines/KeplerApproximation/main.wasm?init'
+
+const arraySize = 2;
+
+init().then((instance) => {
+    console.log(instance.exports)
+    const wasmArray = new Int32Array(instance.exports.memory.buffer, instance.exports.KeplerEngine(2, 4), arraySize);
+    const jsArray = Array.from(wasmArray);
 
     // Print the result to the console
-    console.log("Result:", result);
-};
+    console.log("WASM Array: ", wasmArray);
+    console.log("JS Array: ", jsArray)
+
+    jsArray[0] = 100;
+
+    console.log("WASM Array: ", wasmArray);
+    console.log("JS Array: ", jsArray);
+
+})
